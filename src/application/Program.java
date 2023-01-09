ce5ceff72a27b70988b677367d1b5710589aca5c
davibmberto <davi_berto1@hotmail.com.br>
2023-01-09 04:55:08 -0300

    SellerDao and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Program {
 		Seller sl = new Seller(21, "Bob", "bob@gmail.com",new Date(), 3000.0, dp);
 		
 		System.out.println(sl);
+		
+		SellerDao sld = DaoFactory.createSellerDao();
 		
 	}
 
