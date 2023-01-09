13357983e4ac40c6b465a97b00c473ca435b1f01
davibmberto <davi_berto1@hotmail.com.br>
2023-01-09 04:23:56 -0300

    Seller entity class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,9 +1,13 @@
 package model.entities;
 
-import java.sql.Date;
+import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String name;
@@ -11,15 +15,18 @@ public class Seller {
 	private Date birthDate;
 	private Double baseSalary;
 	
-	private Seller seller;
+	private Department department;
 
-	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Seller seller) {
+	public Seller() {
+	}
+
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
-		this.seller = seller;
+		this.department = department;
 	}
 
 	public Integer getId() {
@@ -62,13 +69,38 @@ public class Seller {
 		this.baseSalary = baseSalary;
 	}
 
-	public Seller getSeller() {
-		return seller;
+	public Department getDepartment() {
+		return department;
 	}
 
-	public void setSeller(Seller seller) {
-		this.seller = seller;
+	public void setDepartment(Department department) {
+		this.department = department;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+	
+	
+	
 	
 }
