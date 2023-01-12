50a06a0ebb1cc16216fc3f8eb465ccf5a1b96707
davibmberto <davi_berto1@hotmail.com.br>
2023-01-12 09:04:00 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,15 @@ public class Program {
 		
 		System.out.println("INSERTED! New id= " + newSeller.getId());
 		
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");;
+		sellerDao.update(seller);
+		System.out.println("UPDATE COMPLETED");
+		
+		
+		
 	}
 
 }
